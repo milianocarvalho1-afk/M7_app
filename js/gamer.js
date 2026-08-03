c29c1144aa49e32a7dad3a489c35d3515f240465
milianocarvalho1-afk <milianocarvalho1@gmail.com>
2026-08-02 23:20:57 -0400

    Create gamer.js

---
+++ js/gamer.js
@@ -0,0 +1,7 @@
+function launchGame(name) {
+  alert('Iniciando Otimização e Executando: ' + name);
+}
+
+document.getElementById('purgeBtn')?.addEventListener('click', () => {
+  alert('Purga de Cache Realizada! +128MB Liberados.');
+});
